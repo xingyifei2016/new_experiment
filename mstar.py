9469dc05d34b4894f0898291fa3d97c1b49dbcdb
Yifei <xingyifei2016@berkeley.edu>
2020-06-29 19:06:31 -0700

    small fixes

--- mstar.py
+++ mstar.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import model as model_
 import torch
 import numpy as np
 import torch.optim as optim
@@ -12,68 +11,32 @@ import random
 from os import listdir
 from os.path import isfile, join
 import argparse
-import pandas as pd
 from pdb import set_trace as st
 from logger import setup_logger
-import resnet_loader as r
-import tenclass as t
-import resnet18 as re
 
+from models import new_models as m
+from dataloaders import tenclass as t
+import matplotlib.pyplot as plt
 
-def data_prep(data_dir, train_batch, test_batch, train_split, logger, validation_threshold=0.9):
-    data_x = []
-    data_y = []
-    for i, f in enumerate(listdir(data_dir)):
-        if i % 1000 == 0:
-            print("Loaded file "+str(i)+" of "+str(len(listdir(data_dir))))
-        data = np.load(join(data_dir, f))
-        label = f.split('_')[0].split('c')[1]
-        if int(label)-1 <= 9:
-            data_x.append(data)
-            data_y.append(int(label)-1)
-    data_x = np.array(data_x)
-    data_y = np.array(data_y)
-    xshape = data_x.shape
-    data_x = data_x.reshape((xshape[0], xshape[1], 1, xshape[2], xshape[3]))
+def plot_grad_flow(named_parameters):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.savefig('books_read.png')
+#     st()
     
     
-#     # New data form
-    mag = data_x[:, 4,...] + 0.5
-    cos_ = data_x[:, 0,...] 
-    sin_ = data_x[:, 1,...] 
-    data_x[:, 0,...] = np.log(mag)
-    data_x[:, 1,...] = cos_
-    data_x[:, 2,...] = sin_
-    data_x = data_x[:, :3,...]
-    
-    data_set_11 = torch.utils.data.TensorDataset(torch.from_numpy(data_x).type(torch.FloatTensor), torch.from_numpy (data_y).type(torch.LongTensor))
-    
-    permutations = torch.randperm(int(len(data_y)))
-    
-    train_idx = permutations[:int(len(data_y) * train_split * validation_threshold)]
-    validation_idx = permutations[int(len(data_y) * train_split * validation_threshold):int(len(data_y) * train_split)]
-    test_idx = permutations[int(len(data_y) * train_split):]
-    data_train = torch.utils.data.Subset(data_set_11,indices=train_idx)
-    data_val = torch.utils.data.Subset(data_set_11,indices=validation_idx)
-    data_test = torch.utils.data.Subset(data_set_11,indices=test_idx)
-    params_train = {'batch_size': train_batch,
-          'shuffle': True,
-          'num_workers': 1}
-    params_val = {'batch_size': test_batch,
-              'shuffle': False,
-              'num_workers': 1}
-    
-    logger.info(str(train_split * 100)+"% data used for training")
-    logger.info(str(validation_threshold * 100)+"% of training data used for validation")
-    train_generator = torch.utils.data.DataLoader(dataset=data_train, **params_train)
-    val_generator = torch.utils.data.DataLoader(dataset=data_val, **params_train)
-    test_generator = torch.utils.data.DataLoader(dataset=data_test, **params_val)
-    return train_generator, val_generator, test_generator 
-
-
-
-
-
 
 def test(model, device, test_loader, logger, epoch):
     test_loss = 0
@@ -106,6 +69,8 @@ def train(model, device, train_loader, optimizer, epoch, logger):
         loss = criterion(out, labels)
         train_loss += loss 
         loss.backward()
+        plot_grad_flow(model.named_parameters())
+        
         optimizer.step()
     print("#####EPOCH "+str(epoch)+"#####")
     print("Train accuracy is: "+str(train_acc / len(train_loader.dataset)*100.))
@@ -122,7 +87,7 @@ def compare_and_save(model, current_acc, highest, save_path, logger, batch, lear
             except:
                 pass
         highest = current_acc
-        save_path = os.path.join('./save/', '[{acc}]-[{batch}]-[{learning_rate}]-11class-model-[{split}]'.format(acc = np.round(current_acc, 3), batch=batch, learning_rate=learn, split=split))
+        save_path = os.path.join('./save/', '[{acc}]-[{batch}]-[{learning_rate}]-[{split}]'.format(acc = np.round(current_acc, 3), batch=batch, learning_rate=learn, split=split))
         torch.save(model.state_dict(), save_path+'.ckpt')
         logger.info('Saved model checkpoints into {}...'.format(save_path))
     return highest, save_path, res
@@ -179,8 +144,9 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 #     batches = [80, 50, 1000, 30, 600] 
     batches = [256]
-    lrs = [0.015, 0.03, 0.005, 0.05]
-    models = {'difference+tangent': model_.Dis_Tan, 'tangent only': model_.NL, 'difference': model_.Dis, 'tangent+difference': model_.Tan_Dis, 'previous_architecture': model_.ManifoldNetRes}
+    lrs = [0.001, 0.0005]#[0.01, 0.005, 0.05]
+#     models = {'difference+tangent': model_.Dis_Tan, 'tangent only': model_.NL, 'difference': model_.Dis, 'tangent+difference': model_.Tan_Dis, 'previous_architecture': model_.ManifoldNetRes}
+    models = {'newModels': m.Model1}
     num_repeat = 2
     splits = [0.1, 0.05, 0.01]
     
@@ -251,7 +217,7 @@ def main():
         logger.info("#Model Parameters: "+str(params))
         for split in splits:
             for i in batches:
-                train_loader, val_loader, test_loader = data_prep(args.data_dir, i, args.test_batchsize, split, logger)
+                train_loader, val_loader, test_loader = t.data_prep(args.data_dir, i, args.test_batchsize, split, logger)
                 
                 for j in lrs:
                     even_higher = 0
