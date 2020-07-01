7bf42139db0a08a6a49ccf1ca08fb9023d22cf93
Yifei <xingyifei2016@berkeley.edu>
2020-06-30 18:52:00 -0700

    S

---
+++ dataloaders/easyloader.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import torch
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils import data
+import random
+import math
+import os
+import random
+from os import listdir
+from os.path import isfile, join
+import argparse
+from pdb import set_trace as st
+from logger import setup_logger
+
+def data_prep(data_dir, train_batch, test_batch, train_split, logger, validation_threshold=0.9):
+    # log(mag), cos(theta), sin(theta)
+    # This is 2 - class
+    data_x = []
+    data_y = []
+    ellipses = np.load(data_dir+'/ellipses.npy')
+    rectangles = np.load(data_dir+'/rectangles.npy')
+    
+    ellipse_labels = np.zeros(len(ellipses))
+    rectangle_labels = np.ones(len(rectangles))
+    
+    ellipses_mag = np.expand_dims(np.log(ellipses[:, 0, ...]), axis=1)
+    ellipses_cos = np.expand_dims(np.cos(ellipses[:, 1, ...]), axis=1)
+    ellipses_sin = np.expand_dims(np.sin(ellipses[:, 1, ...]), axis=1)
+    
+    ellipses_final = np.concatenate((ellipses_mag, ellipses_cos, ellipses_sin), axis=1)
+    
+    rectangles_mag = np.expand_dims(np.log(rectangles[:, 0, ...]), axis=1)
+    rectangles_cos = np.expand_dims(np.cos(rectangles[:, 1, ...]), axis=1)
+    rectangles_sin = np.expand_dims(np.sin(rectangles[:, 1, ...]), axis=1)
+    
+    rectangles_final = np.concatenate((rectangles_mag, rectangles_cos, rectangles_sin), axis=1)
+    
+    data_x = np.concatenate((ellipses_final, rectangles_final), axis=0)
+    data_y = np.concatenate((ellipse_labels, rectangle_labels), axis=0)
+    
+    perm_list = np.random.permutation(int(len(data_x)))
+
+    data_x = data_x[perm_list]
+    data_y = data_y[perm_list]
+    data_set_11 = torch.utils.data.TensorDataset(torch.from_numpy(data_x).type(torch.FloatTensor), torch.from_numpy (data_y).type(torch.LongTensor))
+    
+    permutations = torch.randperm(int(len(data_y)))
+    
+    train_idx = permutations[:int(len(data_y) * train_split * validation_threshold)]
+    validation_idx = permutations[int(len(data_y) * train_split * validation_threshold):int(len(data_y) * train_split)]
+    test_idx = permutations[int(len(data_y) * train_split):]
+    data_train = torch.utils.data.Subset(data_set_11,indices=train_idx)
+    data_val = torch.utils.data.Subset(data_set_11,indices=validation_idx)
+    data_test = torch.utils.data.Subset(data_set_11,indices=test_idx)
+    params_train = {'batch_size': train_batch,
+          'shuffle': True,
+          'num_workers': 1}
+    params_val = {'batch_size': test_batch,
+              'shuffle': False,
+              'num_workers': 1}
+    
+    logger.info(str(train_split * 100)+"% data used for training")
+    logger.info(str(validation_threshold * 100)+"% of training data used for validation")
+    train_generator = torch.utils.data.DataLoader(dataset=data_train, **params_train)
+    val_generator = torch.utils.data.DataLoader(dataset=data_val, **params_train)
+    test_generator = torch.utils.data.DataLoader(dataset=data_test, **params_val)
+    return train_generator, val_generator, test_generator 
---
+++ easy_baseline.py
@@ -0,0 +1,299 @@
+# -*- coding: utf-8 -*-
+import torch
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils import data
+import random
+import math
+import os
+import random
+from os import listdir
+from os.path import isfile, join
+import argparse
+from pdb import set_trace as st
+from logger import setup_logger
+
+from models import new_models as m
+from dataloaders import easyloader as t
+import matplotlib.pyplot as plt
+from models import resnet18 as re
+from models import layers
+def plot_grad_flow(named_parameters):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.savefig('books_read.png')
+#     st()
+    
+    
+
+def test(model, device, test_loader, logger, epoch):
+    test_loss = 0
+    correct = 0
+    pred_all = np.array([[]]).reshape((0, 1))
+    real_all = np.array([[]]).reshape((0, 1))
+    with torch.no_grad():
+        for i, (data, target) in enumerate(test_loader):
+            targets = target.cpu().numpy()
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    print("Test Accuracy is: "+str(100. * correct / len(test_loader.dataset)))
+    logger.info("Test-"+str(epoch)+": "+str(100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset)
+
+def train(model, device, train_loader, optimizer, epoch, logger):
+    train_acc = 0
+    train_loss = 0
+    for it,(local_batch, local_labels) in enumerate(train_loader):
+        batch = torch.tensor(local_batch, requires_grad=True).cuda()
+        labels = local_labels.cuda()
+        optimizer.zero_grad()
+        out = model(batch)
+        _, predicted = torch.max(out, 1)
+        total = labels.shape[0]
+        train_acc += (predicted == labels).sum().item()
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(out, labels)
+        train_loss += loss 
+        loss.backward()
+#         plot_grad_flow(model.named_parameters())
+        
+        optimizer.step()
+    print("#####EPOCH "+str(epoch)+"#####")
+    print("Train accuracy is: "+str(train_acc / len(train_loader.dataset)*100.))
+    print("Train loss is: "+str((train_loss / len(train_loader.dataset)*100.).item()))
+    logger.info("Loss: "+str((train_loss / len(train_loader.dataset)*100.).item())+ " Train-"+str(epoch)+": "+str(train_acc / len(train_loader.dataset)*100.))
+    
+    
+def compare_and_save(model, current_acc, highest, save_path, logger, batch, learn, split):
+    res = current_acc > highest
+    if res:
+        if save_path is not None:
+            try:
+                os.remove(save_path+'.ckpt')
+            except:
+                pass
+        highest = current_acc
+        save_path = os.path.join('./save/', '[{acc}]-[{batch}]-[{learning_rate}]-[{split}]'.format(acc = np.round(current_acc, 3), batch=batch, learning_rate=learn, split=split))
+        torch.save(model.state_dict(), save_path+'.ckpt')
+        logger.info('Saved model checkpoints into {}...'.format(save_path))
+    return highest, save_path, res
+        
+
+import torch
+import time
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+from models import layers
+import math
+from pdb import set_trace as st
+
+def m(x):
+    return True in torch.isnan(x).cpu().detach().numpy()
+
+
+# class Experimental_model(nn.Module):
+#     def __init__(self):
+#         super(Experimental_model, self).__init__()
+#         self.complex_conv1 = layers.ComplexConv(1, 20, (5, 5), (2, 2), num_tied_block=1)
+#         self.distance1 = layers.DifferenceLayer(20, (5, 5), num_tied_block=1)
+#         self.tangentReLU = layers.tangentRELU()
+#         self.complex_conv2 = layers.ComplexConv(20, 20, (5, 5), (2, 2), num_tied_block=1)
+#         self.linear_1 = layers.DistanceTransform(20, (3, 3), num_tied_block=1)
+#         self.l = nn.Linear(20, 2)
+        
+       
+        
+#     def forward(self, x):
+#         x = x.unsqueeze(2)
+#         x = self.complex_conv1(x)
+#         x = self.complex_conv2(x)
+        
+#         x = self.linear_1(x)
+#         b = x.shape[0]
+#         x = x.view(b, -1)
+        
+#         x = self.l(x)
+#         return x
+    
+class Experimental_model(nn.Module):
+    def __init__(self):
+        super(Experimental_model, self).__init__()
+        self.complex_conv1 = nn.Conv2d(3, 20, (5, 5), (2, 2))
+        
+        self.complex_conv2 = nn.Conv2d(20, 20, (5, 5), (2, 2))
+        self.linear_1 = nn.Conv2d(20, 20, (3, 3), (1, 1))
+        self.l = nn.Linear(20, 2)
+        
+       
+        
+    def forward(self, x):
+        x = self.complex_conv1(x)
+#         x = self.distance1(x)
+#         x = self.tangentReLU(x)
+        x = self.complex_conv2(x)
+        
+        x = self.linear_1(x)
+        b = x.shape[0]
+        x = x.view(b, -1)
+        
+        x = self.l(x)
+        return x
+    
+def main():
+    #argparse settings
+    parser = argparse.ArgumentParser(description='PyTorch MSTAR Example') #400 and 0.001
+    parser.add_argument('--batchsize', type=int, default=100, metavar='N',
+                        help='input batch size for training (default: 400)')
+    parser.add_argument('--test_batchsize', type=int, default=400, metavar='N',
+                        help='input batch size for testing (default: 400)')
+    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                        help='number of epochs to train (default: 100)')
+    parser.add_argument('--lr', type=float, default=0.015, metavar='LR',
+                        help='learning rate (default: 0.015)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='Adam momentum (default: 0.9)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--data-dir', type=str, default="./Scripts", metavar='N',
+                        help='where data is stored')
+    parser.add_argument('--use-pretrain', type=int, default=1, metavar='N',
+                        help='Use pretrained model or not')
+    
+    try:
+        os.mkdir('./log')
+    except:
+        pass
+    
+    # For model saving purposes, initializes as 0
+    # If accuracy higher than "higher" then saves the model
+    highest = 0
+    
+    # The actual path to save
+    save_path=None
+    
+    try:
+        os.mkdir('./save')
+    except:
+        pass
+    
+    
+    
+    logger = setup_logger('MSTAR logger')
+    use_1517 = True
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+    device = torch.device("cuda" if use_cuda else "cpu")
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+#     batches = [80, 50, 1000, 30, 600] 
+    batches = [256]
+    lrs = [0.001, 0.05]
+#     models = {'difference+tangent': model_.Dis_Tan, 'tangent only': model_.NL, 'difference': model_.Dis, 'tangent+difference': model_.Tan_Dis, 'previous_architecture': model_.ManifoldNetRes}
+    models = {'newModels': Experimental_model}
+    num_repeat = 2
+    splits = [0.4, 0.3, 0.2]
+    
+   
+    
+    
+    
+    # For running experiments
+    for model1 in models.keys():
+        model = models[model1]().cuda()
+        logger.info(model)
+        model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        logger.info("#Model Parameters: "+str(params))
+        for split in splits:
+            for i in batches:
+                train_loader, val_loader, test_loader = t.data_prep(args.data_dir, i, args.test_batchsize, split, logger)
+                
+                for j in lrs:
+                    even_higher = 0
+                    for ee in range(num_repeat):
+                        optimizer = optim.Adam(model.parameters(), lr=j, eps=1e-8, amsgrad=True)
+                        logger.info("Learning Rate: "+str(j))
+                        logger.info("Batch Size: "+str(i))
+                        logger.info(model1)
+                        logger.info("Repeating trial "+str(ee))
+                        logger.info("Split "+str(split))
+                        highest = 0
+
+                        for epoch in range(1, args.epochs + 1):
+                            train(model, device, train_loader, optimizer, epoch, logger)
+                            acc=test(model, device, val_loader, logger, epoch)
+                            highest, save_path, new_best = compare_and_save(model, acc, highest, save_path, logger, i, j, split)
+
+                            if new_best:
+                                logger.info("####NEW TEST RESULT#####")
+                                acc=test(model, device, test_loader, logger, epoch)
+                                logger.info("####NEW TEST RESULT#####")
+                                even_higher = max(acc, even_higher)
+                        acc=test(model, device, test_loader, logger, epoch)
+                        even_higher = max(acc, even_higher)
+                        logger.info("########## NEW MODEL ###########")
+                        model = models[model1]().cuda()
+                    logger.info("####Highest Testing Accuracy: "+str(even_higher))
+                    
+                    
+    model = re.ResNet().cuda()
+    logger.info(model)
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    logger.info("#Model Parameters: "+str(params))
+    for split in splits:
+        for i in batches:
+            train_loader, val_loader, test_loader = t.sqrt_prep(args.data_dir, i, args.test_batchsize, split, logger)
+
+            for j in lrs:
+                even_higher = 0
+                for ee in range(num_repeat):
+                    optimizer = optim.Adam(model.parameters(), lr=j, eps=1e-8, amsgrad=True)
+                    logger.info("Learning Rate: "+str(j))
+                    logger.info("Batch Size: "+str(i))
+                    logger.info("ResNet18")
+                    logger.info("Repeating trial "+str(ee))
+                    logger.info("Split "+str(split))
+                    highest = 0
+
+                    for epoch in range(1, args.epochs + 1):
+                        train(model, device, train_loader, optimizer, epoch, logger)
+                        acc=test(model, device, val_loader, logger, epoch)
+                        highest, save_path, new_best = compare_and_save(model, acc, highest, save_path, logger, i, j, split)
+
+                        if new_best:
+                            logger.info("####NEW TEST RESULT#####")
+                            acc=test(model, device, test_loader, logger, epoch)
+                            logger.info("####NEW TEST RESULT#####")
+                            even_higher = max(acc, even_higher)
+                    acc=test(model, device, test_loader, logger, epoch)
+                    even_higher = max(acc, even_higher)
+                    logger.info("########## NEW MODEL ###########")
+                    model = re.ResNet().cuda()
+                logger.info("####Highest Testing Accuracy: "+str(even_higher))
+
+if __name__ == '__main__':
+    main()
+       
+
